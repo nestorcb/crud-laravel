49cf74b7589e4c4a6868f5a027e24de493eaed12
nestorcb <nestorcb.91@gmail.com>
2019-05-24 18:42:14 +0100

    Fix create contact
    Contact migration file has contact model fields.
    Database updated accordingly.

--- database/migrations/2019_05_24_173706_contacts.php
+++ database/migrations/2019_05_24_173706_contacts.php
@@ -16,6 +16,12 @@ class Contacts extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('job_title');
+            $table->string('city');   
+            $table->string('country');   
         });
     }
 
